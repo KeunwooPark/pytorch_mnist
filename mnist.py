7e3b74cc2766cb3bade7931926f15219a63d268c
Keunwoo Park <avolic@kaist.ac.kr>
2018-06-29 11:17:34 +0900

    fix wrong url

--- mnist.py
+++ mnist.py
@@ -16,7 +16,7 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(50, 10)
     
     def forward(self, x):
-        # Difference between nn.funcional vs. nn -> https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel.html
+        # Difference between nn.funcional vs. nn -> https://discuss.pytorch.org/t/whats-the-difference-between-torch-nn-functional-and-torch-nn/681
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
         x = x.view(-1, 320)
